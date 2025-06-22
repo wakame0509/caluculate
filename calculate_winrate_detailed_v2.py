27bd80571955a55e57f3b9b740c4d88ec3d83b56
wakame0509 <wakamegek1699@yahoo.ne.jp>
2025-06-22 22:02:33 +0900

    Update calculate_winrate_detailed_v2.py

--- calculate_winrate_detailed_v2.py
+++ calculate_winrate_detailed_v2.py
@@ -2,7 +2,7 @@ import eval7
 import random
 from extract_features import extract_features_for_flop, extract_features_for_turn, extract_features_for_river
 from preflop_winrate_dict import get_static_preflop_winrate
-from hand_utils import detect_made_hand  # ★ 追加
+from hand_utils import detect_made_hand  # 役を取得
 
 def calculate_equity(hero, board, opp_hands, iters=1000):
     hero_wins = 0
@@ -46,49 +46,67 @@ def simulate_shift_turn_with_ranking(hand_str, flop_list, opp_hands, iters=1000)
     hero = [eval7.Card(hand_str[0:2]), eval7.Card(hand_str[2:4])]
     total_shift = 0
     all_turns = []
+
     for flop in flop_list:
         flop_cards = [eval7.Card(c) for c in flop]
         deck = eval7.Deck()
         used = hero + flop_cards
         for card in used:
             deck.cards.remove(card)
-        turn_shifts = []
+
         for turn in deck:
             board = flop_cards + [turn]
             equity = calculate_equity(hero, board, opp_hands, iters)
             shift = equity - calculate_equity(hero, flop_cards, opp_hands, iters)
             features = extract_features_for_turn(board)
-            turn_shifts.append((str(turn), shift, features))
-        all_turns += turn_shifts
-    all_turns.sort(key=lambda x: x[1], reverse=True)
+            made_hand = detect_made_hand(hero, board)
+
+            all_turns.append({
+                'turn_card': str(turn),
+                'shift': shift,
+                'features': features,
+                'role': made_hand[0]
+            })
+
+    all_turns.sort(key=lambda x: x['shift'], reverse=True)
     top10 = all_turns[:10]
     worst10 = all_turns[-10:]
-    avg_total = sum([s[1] for s in all_turns]) / len(all_turns)
+    avg_total = sum([x['shift'] for x in all_turns]) / len(all_turns)
     return avg_total, top10, worst10
 
 def simulate_shift_river_with_ranking(hand_str, flop_list, opp_hands, iters=1000):
     hero = [eval7.Card(hand_str[0:2]), eval7.Card(hand_str[2:4])]
     total_shift = 0
     all_rivers = []
+
     for flop in flop_list:
         flop_cards = [eval7.Card(c) for c in flop]
         deck = eval7.Deck()
         used = hero + flop_cards
         for card in used:
             deck.cards.remove(card)
+
         turn = random.choice(deck)
         used.append(turn)
         for card in used:
             deck.cards.remove(card)
+
         for river in deck:
             board = flop_cards + [turn, river]
             equity = calculate_equity(hero, board, opp_hands, iters)
             shift = equity - calculate_equity(hero, flop_cards + [turn], opp_hands, iters)
             features = extract_features_for_river(board)
-            made_hand = detect_made_hand(hero, board)  # ★ 役を検出
-            all_rivers.append((str(river), shift, features, made_hand[0]))  # ★ 役も追加
-    all_rivers.sort(key=lambda x: x[1], reverse=True)
+            made_hand = detect_made_hand(hero, board)
+
+            all_rivers.append({
+                'river_card': str(river),
+                'shift': shift,
+                'features': features,
+                'role': made_hand[0]
+            })
+
+    all_rivers.sort(key=lambda x: x['shift'], reverse=True)
     top10 = all_rivers[:10]
     worst10 = all_rivers[-10:]
-    avg_total = sum([s[1] for s in all_rivers]) / len(all_rivers)
+    avg_total = sum([x['shift'] for x in all_rivers]) / len(all_rivers)
     return avg_total, top10, worst10
