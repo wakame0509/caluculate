84be0000a148e17e7402b018d9f65aac31e4a59d
wakame0509 <wakamegek1699@yahoo.ne.jp>
2025-06-29 00:43:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ if mode == "プリフロップ勝率生成":
         st.download_button("CSVダウンロード", result_df.to_csv(index=False), "preflop_winrates.csv", "text/csv")
     st.stop()
 if mode in ["自動生成モード", "手動選択モード"]:
-        ALL_HANDS = all_starting_hands()  
+        ALL_HANDS = all_starting_hands 
         hand_str = st.selectbox("自分のハンドを選択してください", ALL_HANDS)
 # 自動モード
 if mode == "自動生成モード":
